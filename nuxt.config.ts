f28171ed807e6d1800ce27d69045e7bf3753562e
iamvetle <vetle122@gmail.com>
2024-07-16 15:40:23 +0200

    Configure newest nuxt version with the newest compatability.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules:[
-    "@vueuse/nuxt",
-    '@nuxtjs/tailwindcss',
-  ],
-  nitro: {
-    prerender: {
-      // Have to specify this (npx nuxi generate) or manifest.json will become a html file..
-      ignore: [
-        "/manifest.json"
-      ]
-    }
-  }
-})
+	devtools: { enabled: true },
+	modules: ["@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+
+	nitro: {
+		prerender: {
+			// Have to specify this (npx nuxi generate) or manifest.json will become a html file..
+			ignore: ["/manifest.json"],
+		},
+	},
+
+	future: { compatibilityVersion: 4 },
+	compatibilityDate: "2024-07-16",
+});

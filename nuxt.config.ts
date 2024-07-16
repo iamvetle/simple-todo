b02dedb1fc95d95212a76e2ebbf81750b827055d
iamvetle <vetle122@gmail.com>
2024-07-16 17:08:01 +0200

    Fix vercel problem.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,24 +23,24 @@ export default defineNuxtConfig({
 			background_color: "#ffffff",
 			icons: [
 				{
-					src: "android-chrome-192x192.png",
+					src: "android/android-chrome-192x192.png",
 					sizes: "192x192",
 					type: "image/png",
 					purpose: "maskable",
 				},
 				{
-					src: "android-chrome-512x512.png",
+					src: "android/android-chrome-512x512.png",
 					sizes: "512x512",
 					type: "image/png",
 					purpose: "maskable",
 				},
 				{
-					src: "android-chrome-192x192.png",
+					src: "android/android-chrome-192x192.png",
 					sizes: "192x192",
 					type: "image/png",
 				},
 				{
-					src: "android-chrome-512x512.png",
+					src: "android/android-chrome-512x512.png",
 					sizes: "512x512",
 					type: "image/png",
 				},
